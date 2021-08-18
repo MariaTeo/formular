75cc37aae8f9fe1f769a869ca5e41edb290cf39a
Maria Teo <marya.teodoraa@gmail.com>
2021-08-18 22:39:02 +0100

    working on form validation

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-console.log('merge?')
+const form1 = document.getElementById("form1");
+const form2 = document.getElementById("form2");
+const btn1 = document.getElementById("first_btn");
+const btn2 = document.getElementById("lastF");
+const inputFname = document.getElementById("fname")
+const inputLname = document.getElementById("lname")
+const inputEmail = document.getElementById("email")
+const arr = [inputEmail, inputFname, inputLname]
 
-const form1 = document.getElementById("form1")
+btn1.addEventListener("click", function (e) {
+  displayForm(form1, form2);
+  formVAlidation(arr)
+  e.preventDefault();
+});
 
-const form2 = document.getElementById
-("form2")
-const btn1 = document.getElementById('first_btn')
-
-btn1.addEventListener("click", oriceNume())
+btn2.addEventListener("click", function (e) {
+  displayForm(form2, form1), e.preventDefault();
+});
 
 function displayForm(a, b) {
-  a.style.display = "block"
-  b.style.display = "none"
+  a.style.visibility = "hidden";
+  b.style.visibility = "visible";
 }
 
-function oriceNume(e) {
-  displayForm(form1, form2)
-  e.preventDefault()
+
+function formVAlidation(arr) {
+  for(let i = 0; i<arr; i++) {
+    console.log(arr, "merge")
+  }
 }
+
